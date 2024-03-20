99ef8f08be3314cac0dba424f6596e62c9ba3a46
Anuska <anuska.maharana2003@gmail.com>
2024-03-20 18:32:05 +0530

    edited

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     city = input("Enter the name of the city:\n")
     url = f"https://api.weatherapi.com/v1/current.json?key=9caf41771ed6472c989173134241903&q={city}"
     r = requests.get(url)
-    # print(r.text)  # type=str
+    # print(r.text) type=str
     wdic = json.loads(r.text)  # parsed into dict
     w = wdic["current"]["temp_c"]
     engine.say(f"The current weather in {city} is {w} degrees.")
