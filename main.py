173a6f36e16dfe3539e73c177e3d161c1b1125d8
Anuska <anuska.maharana2003@gmail.com>
2024-03-20 18:47:11 +0530

    edited

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # print(r.text) type=str
     wdic = json.loads(r.text)  # parsed into dict
     w = wdic["current"]["temp_c"]
-    c=wdic["current"]["condition"]["text"]
+    c = wdic["current"]["condition"]["text"]
 
     engine.say(f"The current weather in {city} is {w} degrees and you will experience {c}.")
     engine.runAndWait()
